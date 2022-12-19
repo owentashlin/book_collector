39c0f1c2311d03c1d94fabc1a7b51fcea923ce0f
owentashlin <owentashlin@gmail.com>
2022-12-19 14:06:58 -0800

    first commit

---
+++ main_app/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from .models import Book
+
+# Create your views here.
+def home(request):
+  return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html') 
+
+def books_index(request):
+  books = Book.objects.all()
+  return render(request, 'books/index.html', {'books':books})
+
+def books_detail(request, book_id):
+  book = Book.objects.get(id=book_id)
+  return render(request, 'books/detail.html', {'book':book})
+
