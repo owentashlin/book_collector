98aa357a3c8a418149137f9d448d86f0b63071e6
owentashlin <owentashlin@gmail.com>
2022-12-19 17:03:12 -0800

    CRUD working on book model, adding Reading model

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Book(models.Model):  
@@ -6,3 +7,14 @@ class Book(models.Model):
     author = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     rating = models.IntegerField()
+    
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'book_id': self.id})
+
+class Reading(models.Model):
+    date: models.DateField()
+    page: models.IntegerField()
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
-
 # Create your views here.
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Book
 
 # Create your views here.
@@ -19,3 +18,16 @@ def books_detail(request, book_id):
   book = Book.objects.get(id=book_id)
   return render(request, 'books/detail.html', {'book':book})
 
+class BookCreate(CreateView):
+  model = Book
+  fields = '__all__'
+  success_url= '/books/'
+
+class BookUpdate(UpdateView):
+  model = Book
+  fields = ['description', 'rating']
+  success_url= '/books/'
+
+class BookDelete(DeleteView):
+  model = Book
+  success_url = '/books/'
